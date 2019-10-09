949b935c7d0420c351ccddab1afe637768c6395c
IngTraining <joydeep.j@hcl.com>
2019-10-09 07:22:20 +0530

    test

--- src/main/java/com/hcl/mortgage/MortgageApplication.java
+++ src/main/java/com/hcl/mortgage/MortgageApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MortgageApplication {
 	
 	//Comments added by dev2
+	//comments added by dev1
 
 	public static void main(String[] args) {
 		SpringApplication.run(MortgageApplication.class, args);
