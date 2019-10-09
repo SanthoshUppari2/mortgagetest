824a6d1878737d637a15dcebde541a310ec52aa5
SanthoshUppari2 <41367381+SanthoshUppari2@users.noreply.github.com>
2019-10-09 07:18:53 +0530

    Update MortgageApplication.java

--- src/main/java/com/hcl/mortgage/MortgageApplication.java
+++ src/main/java/com/hcl/mortgage/MortgageApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MortgageApplication {
+	
+	//Comments added by dev2
 
 	public static void main(String[] args) {
 		SpringApplication.run(MortgageApplication.class, args);
